cbeba6ec8a90e95210b3961860bc5c1712192a7d
Daniel Davenport <ddavenportwork10@gmail.com>
2022-07-03 19:37:59 -0700

    Initial commit

---
+++ RegisterAndLoginApp/Services/SecurityService.cs
@@ -0,0 +1,14 @@
+﻿using RegisterAndLoginApp.Models;
+
+namespace RegisterAndLoginApp.Services
+{
+    public class SecurityService
+    {
+       SecurityDAO securityDAO = new();
+
+        public bool IsValid(UserModel user)
+        {
+            return securityDAO.FindUserByNameAndPassword(user);
+        }
+    }
+}
